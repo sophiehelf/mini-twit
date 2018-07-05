43ae963024a5e734b4a4df23b577818a9c1d688a
Sophie <sophie.helf@gmail.com>
2018-07-04 18:43:51 -0700

    centered footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Container from './Container'
+import Footer from './Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Container />
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+const Footer = (props) => {
+	return(
+		<p className="footer">Raw insight without the horse shit</p>
+	)
+}
+
+export default Footer
